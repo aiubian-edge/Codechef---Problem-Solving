7c9d91685497179b12a1b92ebf816fc87f8f67cf
A . S . M . RADWAN <50805604+radwanromy@users.noreply.github.com>
2022-11-11 01:41:29 +0600

    CCPS 005

---
+++ Volume 01/PRIZEPOOL.cpp
@@ -0,0 +1,36 @@
+// Problem
+// In a coding contest, there are prizes for the top rankers. The prize scheme is as follows:
+
+// Top 1010 participants receive rupees XX each.
+// Participants with rank 1111 to 100100 (both inclusive) receive rupees YY each.
+// Find the total prize money over all the contestants.
+
+// Input Format
+// First line will contain TT, number of test cases. Then the test cases follow.
+// Each test case contains of a single line of input, two integers XX and YY - the prize for top 1010 rankers and the prize for ranks 1111 to 100100 respectively.
+// Output Format
+// For each test case, output the total prize money over all the contestants.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t,x,y;
+	cin>>t;
+	if(t>=1 && t<=100){
+	for(int i=0;i<t;i++){
+        cin>>x>>y;
+        if(x>=1 && x<=1000 && y>=1 && y<=1000 && y<=x){
+        cout<<x*10+y*90<<endl;
+    }
+    else{
+    cout<<"Constrains are not matching!";
+    }
+    }
+    }
+    else{
+    cout<<"Constrains are not matching!";
+    }
+	// your code goes here
+	return 0;
+}
