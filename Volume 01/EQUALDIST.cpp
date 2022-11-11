406d368e9773c95aa3c0436b4d9568590f51c4e8
A . S . M . RADWAN <50805604+radwanromy@users.noreply.github.com>
2022-11-11 09:39:24 +0600

    CC PS 014

---
+++ Volume 01/EQUALDIST.cpp
@@ -0,0 +1,44 @@
+// Problem
+// Alice and Bob are very good friends and they always distribute all the eatables equally among themselves.
+
+// Alice has AA chocolates and Bob has BB chocolates. Determine whether Alice and Bob can distribute all the chocolates equally among themselves.
+
+// Note that:
+
+// It is not allowed to break a chocolate into more than one piece.
+// No chocolate shall be left in the distribution.
+// Input Format
+// The first line of input will contain an integer TT — the number of test cases. The description of TT test cases follows.
+// The first and only line of each test case contains two space-separated integers AA and BB, the number of chocolates that Alice and Bob have, respectively.
+// Output Format
+// For each test case, output on a new line \texttt{YES}YES if Alice and Bob can distribute all the chocolates equally, else output \texttt{NO}NO. The output is case insensitive, i.e, \texttt{yes}yes, \texttt{YeS}YeS, \texttt{yES}yES will all be accepted as correct answers when Alice and Bob can distribute the chocolates equally.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t,y,x;
+    cin>>t;
+    if(t>=1 && t<=1000)
+    {
+    for(int i=0;i<t;i++){
+        cin>>x>>y;
+        if(x>=1 && x<=100000 && y>=1 && y<=100000){
+           if((x+y)%2==0){
+               cout<<"YES"<<endl;
+           }
+           else{
+                cout<<"NO"<<endl;
+           }
+        }
+        else{
+        cout<<"Constraints Errors";
+        }
+    }
+    }
+    else{
+        cout<<"Constraints Errors";
+    }
+	// your code goes here
+	return 0;
+}
