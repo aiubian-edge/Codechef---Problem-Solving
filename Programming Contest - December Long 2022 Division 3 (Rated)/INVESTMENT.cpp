84b3a9f9265299026347f08a17d84d52df17ba20
A . S . M . RADWAN <50805604+radwanromy@users.noreply.github.com>
2022-12-11 10:57:50 +0600

    CC C++ PS DEC221C 02

---
+++ Programming Contest - December Long 2022 Division 3 (Rated)/INVESTMENT.cpp
@@ -0,0 +1,36 @@
+/******************************************************************************
+Good Investment or Not
+
+Problem
+Chef has invested his money at an interest rate of XX percent per annum while the current inflation rate is YY percent per annum.
+
+An investment is called good if and only if the interest rate of the investment is at least twice of the inflation rate.
+Determine whether the investment made by Chef is good or not.
+
+Input Format
+The first line of input will contain a single integer TT, denoting the number of test cases.
+Each test case consists of two integers XX and YY, the interest rate and the current inflation rate respectively.
+Output Format
+For each test case, output YES if the investment is good, NO otherwise.
+
+You can output any letter in any case. For example YES, yes, yES are all considered same.
+
+*******************************************************************************/
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int x,y;
+        cin>>x>>y;
+        if(x>=2*y){
+            cout<<"YES"<<endl;
+        }else{
+            cout<<"NO"<<endl;
+        }
+    }
+	// your code goes here
+	return 0;
+}
