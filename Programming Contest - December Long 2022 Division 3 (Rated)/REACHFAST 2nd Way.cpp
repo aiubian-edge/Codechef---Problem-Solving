b643413e5963f5d6a67f7abab3860cb89376659f
A . S . M . RADWAN <50805604+radwanromy@users.noreply.github.com>
2022-12-11 10:01:24 +0600

    CC C++ PS DEC221C 01 02

---
+++ Programming Contest - December Long 2022 Division 3 (Rated)/REACHFAST 2nd Way.cpp
@@ -0,0 +1,35 @@
+/******************************************************************************
+Reach fast
+
+Problem
+Chef is standing at coordinate AA while Chefina is standing at coordinate BB.
+
+In one step, Chef can increase or decrease his coordinate by at most KK.
+
+Determine the minimum number of steps required by Chef to reach Chefina.
+
+Input Format
+The first line of input will contain a single integer TT, denoting the number of test cases.
+Each test case consists of three integers X, Y,X,Y, and KK, the initial coordinate of Chef, the initial coordinate of Chefina and the maximum number of coordinates Chef can move in one step.
+Output Format
+For each test case, output the minimum number of steps required by Chef to reach Chefina.
+
+*******************************************************************************/
+#include <iostream>
+#include<cmath>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        float x,y,k;
+        cin>>x>>y>>k;
+        float d = abs(x-y);
+        float a= ceil(d/k);
+        cout<<a<<endl;
+    }
+	// your code goes here
+	return 0;
+}
+
